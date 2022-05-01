981028a3090dde9eb58c6781569c2b25f378e6c4
shivamdubey715 <81400047+shivamdubey715@users.noreply.github.com>
2022-05-01 16:35:36 +0530

    Update Maze_Obstacle.cpp

--- Dynamic Programming/Maze_Obstacle.cpp
+++ Dynamic Programming/Maze_Obstacle.cpp
@@ -1,6 +1,7 @@
 /*
-We are given an “N*M” Maze. The maze contains some obstacles. A cell is ‘blockage’ in the maze if its value is -1. 0 represents non-blockage. There is no path possible through a blocked cell.
-We need to count the total number of unique paths from the top-left corner of the maze to the bottom-right corner. At every cell, we can move either down or towards the right.
+We are given an “N*M” Maze. The maze contains some obstacles. A cell is ‘blockage’ in the maze if its value is -1. 
+0 represents non-blockage. There is no path possible through a blocked cell.We need to count the total number of unique paths from the top-left corner of the
+maze to the bottom-right corner. At every cell, we can move either down or towards the right.
 */
 
 #include <bits/stdc++.h>
